a71ecb8b0f06537b2cffc849b4cdf68149514d6a
elyes <elyesbossmessous2022@gmail.com>
2023-04-07 21:57:45 +0100

    added

---
+++ main.js
@@ -0,0 +1,15 @@
+let my =["ahmed","ali","elyes"]
+console.log(my)
+my[3]="kig"
+console.log(my)
+my[4]="k4574"
+console.log(my)
+
+
+my [ my.length ]="pepe" ;
+console.log(my);
+my.unshift("b3b3i")
+console.log(my)
+my.push("fddd")
+console.log(my)
+console.log(my.length)
